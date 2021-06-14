ecbee86da2ff80d3d980a3c9704a74b0e59e9840
santiiiespinoza1 <55319567+santiiiespinoza1@users.noreply.github.com>
2021-06-14 06:08:46 -0300

    Update pasar-pagina.js

--- pasar-pagina.js
+++ pasar-pagina.js
@@ -1,30 +1,40 @@
 const arrowRight = document.querySelector('.right');
 const arrowLeft = document.querySelector('.left');
-const imagen = document.querySelector('.catalogo')
+const imagen = document.querySelector('.catalogo');
+const pagActual = document.querySelector('.pag-actual');
 var posActual = 1;
 arrowRight.addEventListener('click',function(){
-    posActual++;
-    arrowRight.style.background = '#aaa';
+    if(posActual === 3){
+        posActual= 1;
+    }else{
+        posActual++;
+    }
+
+    arrowRight.style.background = 'rgb(50,50,50,.5)';
     setTimeout(function(){
-        arrowRight.style.background = 'rgb(155,155,155,.5)';
+        arrowRight.style.background = 'rgb(155,155,155,.3)';
     },500,"JavaScript");
     
     imagen.style.background = `url(img/pag${posActual}.jpg)`;
     imagen.style.backgroundSize = 'contain';
     imagen.style.backgroundRepeat = 'no-repeat';
     imagen.style.backgroundPosition = 'center';
+    pagActual.innerHTML = `Pag. ${posActual}/3`;
     console.log(posActual);
-    if(posActual === 3){
-        posActual= 0;
-    }
+    
 
 })
 
 arrowLeft.addEventListener('click',function(){
-   
-    arrowLeft.style.background = '#aaa';
+    
+    if(posActual === 1){
+        posActual = 3;
+    }else{
+        posActual--;
+    }
+    arrowLeft.style.background = 'rgb(50,50,50,.5)';
     setTimeout(function(){
-        arrowLeft.style.background = 'rgb(155,155,155,.5)';
+        arrowLeft.style.background = 'rgb(155,155,155,.3)';
     },500,"JavaScript");
     
     imagen.style.background = `url(img/pag${posActual}.jpg)`;
@@ -32,10 +42,7 @@ arrowLeft.addEventListener('click',function(){
     imagen.style.backgroundRepeat = 'no-repeat';
     imagen.style.backgroundPosition = 'center';
     
-    if(posActual === 1){
-        posActual = 4;
-    }
-    posActual--;
-    console.log(posActual);
 
+    pagActual.innerHTML = `Pag. ${posActual}/3`;
+    console.log(posActual);
 })
